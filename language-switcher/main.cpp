a05c68ca072ac021f6173beb35cb2193db1367ae
Korn Rojrattanapanya <kornkubzaza@gmail.com>
2026-03-07 03:02:16 +0700

    Update version 1.0.3

--- language-switcher/main.cpp
+++ language-switcher/main.cpp
@@ -1,120 +1,120 @@
 #include <stdbool.h>
 #include <windows.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <tchar.h>
-#include <time.h>
 
-UINT	toggle_key = VK_CAPITAL;
-HHOOK	handle_hook;
-HANDLE	handle_event;
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 
-bool	is_key_hold = false, is_key_execute = false;
-bool	is_caplock = false;
-clock_t	key_start, key_end;
-double	key_total;
+UINT toggle_key = VK_CAPITAL;
+HHOOK handle_hook;
+HANDLE handle_event;
+UINT_PTR timer_id;
 
-LRESULT CALLBACK keyboard_hook(int n_code, WPARAM w_param, LPARAM l_param) {
+bool is_key_hold = false, is_key_execute = false;
+bool is_caplock = false;
+ULONGLONG key_start;
+
+static void SendKey(WORD vk_code, bool is_down) {
+	INPUT input = { 0 };
+	input.type = INPUT_KEYBOARD;
+	input.ki.wVk = vk_code;
+	input.ki.dwFlags = is_down ? 0 : KEYEVENTF_KEYUP;
+
+	if (vk_code == VK_LWIN) {
+		input.ki.dwFlags |= KEYEVENTF_EXTENDEDKEY;
+	}
+
+	SendInput(1, &input, sizeof(INPUT));
+}
+
+static LRESULT CALLBACK keyboard_hook(int n_code, WPARAM w_param, LPARAM l_param) {
 	if (n_code == HC_ACTION) {
-		KBDLLHOOKSTRUCT* s_keyboard_hook = (KBDLLHOOKSTRUCT*) l_param;
-		HWND handle_window = GetForegroundWindow();
+		KBDLLHOOKSTRUCT* s_keyboard_hook = (KBDLLHOOKSTRUCT*)l_param;
+
 		if (s_keyboard_hook->vkCode == toggle_key && !is_caplock) {
-			if (w_param == WM_KEYDOWN && handle_window != NULL && !is_key_hold) {
-				is_key_hold = true;
-				key_start = clock();
-			}
-			if (w_param == WM_KEYDOWN && handle_window != NULL && is_key_hold && !is_key_execute) {
-				key_end = clock();
-				key_total = (double)(key_end - key_start) / CLOCKS_PER_SEC;
 
-				if (key_total >= 1) {
-					is_key_execute = true;
-
-					is_caplock = true;
-					keybd_event(VK_CAPITAL, 0x45, KEYEVENTF_EXTENDEDKEY|0, 0);
-					keybd_event(VK_CAPITAL, 0x45, KEYEVENTF_EXTENDEDKEY|KEYEVENTF_KEYUP, 0);
-					is_caplock = false;
+			if (w_param == WM_KEYDOWN || w_param == WM_SYSKEYDOWN) {
+				if (!is_key_hold) {
+					is_key_hold = true;
+					key_start = GetTickCount64();
 				}
+				else if (!is_key_execute) {
+					if ((GetTickCount64() - key_start) >= 1000) {
+						is_key_execute = true;
+						is_caplock = true;
+
+						SendKey(VK_CAPITAL, true);
+						SendKey(VK_CAPITAL, false);
+
+						is_caplock = false;
+					}
+				}
+				return 1;
 			}
-			if (w_param == WM_KEYUP && handle_window != NULL) {
-				key_end = clock();
-				key_total = (double)(key_end - key_start) / CLOCKS_PER_SEC;
-				
+
+			if (w_param == WM_KEYUP || w_param == WM_SYSKEYUP) {
+				if (is_key_hold && !is_key_execute) {
+					if ((GetTickCount64() - key_start) < 1000) {
+						SendKey(VK_LWIN, true);
+						SendKey(VK_SPACE, true);
+						SendKey(VK_SPACE, false);
+						SendKey(VK_LWIN, false);
+					}
+				}
 				is_key_hold = false;
 				is_key_execute = false;
-				is_caplock = false;
-				if (key_total < 1) {
-					PostMessage(handle_window, WM_INPUTLANGCHANGEREQUEST, 0, HKL_NEXT);
-				}
+
+				return 1;
 			}
-			return 1;
 		}
 	}
 	return CallNextHookEx(handle_hook, n_code, w_param, l_param);
 }
 
-void CALLBACK timer_callback(HWND handle_window, UINT u_message, UINT_PTR event_id, DWORD dw_time) {
+static void CALLBACK timer_callback(HWND handle_window, UINT u_message, UINT_PTR event_id, DWORD dw_time) {
 	if (WaitForSingleObject(handle_event, 0) == WAIT_OBJECT_0) {
 		PostQuitMessage(0);
 	}
 }
 
-int main(int argc, char** argv) {
-	ShowWindow(FindWindowA("ConsoleWindowClass", NULL), false);
-	FreeConsole();
-
-	bool is_quit = false;
-	
-	if (argc > 2) {
-		MessageBox(NULL, _T("Invalid number of arguments"), _T("Error"), MB_OK|MB_ICONERROR);
-		printf("ERROR: invalid number of arguments.");
-	}
-	if (argc == 1) {
-		is_quit = true;
-	}
-	if (argc == 2) {
-		toggle_key = atoi(argv[1]);
-	}
-
+int main() {
 	handle_event = CreateEvent(NULL, true, false, _T("Language Switcher"));
 	if (handle_event == NULL) {
-		MessageBox(NULL, _T("Unable to start a program"), _T("Error"), MB_OK|MB_ICONERROR);
-		printf("ERROR: unable to start a program.");
+		MessageBox(NULL, _T("Unable to start the program."), _T("Error"), MB_OK | MB_ICONERROR);
+		
 		return 1;
 	}
+
 	if (GetLastError() == ERROR_ALREADY_EXISTS) {
-		if (is_quit) {
-			SetEvent(handle_event);
-			CloseHandle(handle_event);
-			MessageBox(NULL, _T("Program is ended"), _T("Success"), MB_OK|MB_ICONINFORMATION);
-			printf("SUCCESS: program is ended.");
-			return 0;
-		}
-		else {
-			MessageBox(NULL, _T("Program is already running..."), _T("Error"), MB_OK|MB_ICONERROR);
-			printf("ERROR: program is already running...");
-			return 1;
-		}
+		SetEvent(handle_event);
+		CloseHandle(handle_event);
+		MessageBox(NULL, _T("Program is ended."), _T("Success"), MB_OK | MB_ICONINFORMATION);
+		
+		return 0;
 	}
-	
-	if (SetTimer(NULL, 0, 1000, timer_callback) == NULL) {
-		MessageBox(NULL, _T("Unable to start a program"), _T("Error"), MB_OK|MB_ICONERROR);
-		printf("ERROR: unable to start a program.");
+
+	timer_id = SetTimer(NULL, 0, 1000, timer_callback);
+	if (timer_id == 0) {
+		MessageBox(NULL, _T("Unable to create timer."), _T("Error"), MB_OK | MB_ICONERROR);
+		
 		return 1;
 	}
 
 	handle_hook = SetWindowsHookEx(WH_KEYBOARD_LL, keyboard_hook, GetModuleHandle(NULL), 0);
 	if (handle_hook == NULL) {
-		MessageBox(NULL, _T("Unable to start a program"), _T("Error"), MB_OK|MB_ICONERROR);
-		printf("ERROR: unable to start a program.");
+		MessageBox(NULL, _T("Unable to install hook."), _T("Error"), MB_OK | MB_ICONERROR);
+		
 		return 1;
 	}
 
 	MSG msg;
-	while (GetMessage(&msg,0,0,0)) {
+	while (GetMessage(&msg, NULL, 0, 0)) {
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
+
+	UnhookWindowsHookEx(handle_hook);
+	KillTimer(NULL, timer_id);
+	CloseHandle(handle_event);
 
 	return 0;
 }
